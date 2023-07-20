7d2564bc67a39a637f40f1d3f7e986fb0546441f
Zahir Hadi Athallah <89567774+Jahirrrr@users.noreply.github.com>
2023-07-20 14:27:54 +0700

    Add files via upload

---
+++ client/src/components/logo/Logo.js
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import { forwardRef } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Link } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+const Logo = forwardRef(({disabledLink = false, sx, ...other}, ref) => {
+
+  const logo = (
+    <Box
+      ref={ref}
+      component="div"
+      sx={{
+        display: 'inline-flex',
+        textAlign: 'center',
+        margin: 'auto',
+        justifyContent: 'center',
+        ...sx,
+      }}
+      {...other}
+    >
+      <img src="./assets/logolib.png" alt="Logo" width="80%"/>
+    </Box>
+  );
+
+  if (disabledLink) {
+    return <>{logo}</>;
+  }
+
+  return (
+    <Link to="/" component={RouterLink} sx={{display: 'contents'}}>
+      {logo}
+    </Link>
+  );
+});
+
+Logo.propTypes = {
+  sx: PropTypes.object,
+  disabledLink: PropTypes.bool,
+};
+
+export default Logo;

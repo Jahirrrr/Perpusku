468ee94e4dcbef26b292a22bc08f7bb39610ed8e
Zahir Hadi Athallah <89567774+Jahirrrr@users.noreply.github.com>
2023-07-20 14:23:26 +0700

    Add files via upload

---
+++ client/src/constants.js
@@ -0,0 +1,23 @@
+const backendApiUrl = "http://localhost:8080/api";
+
+const routes = {
+  AUTHOR: "author",
+  AUTH: "auth",
+  BOOK: "book",
+  BORROWAL: "borrowal",
+  BOOKRETURN: "bookReturn",
+  GENRE: "genre",
+  USER: "user"
+};
+
+const methods = {
+  GET: "get",
+  GET_ALL: "getAll",
+  POST: "add",
+  PUT: "update",
+  DELETE: "delete"
+};
+
+const apiUrl = (route, method, id = "") => `${backendApiUrl}/${route}/${method}${id && `/${id}`}`;
+
+module.exports = { routes, methods, apiUrl };
---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "./hooks/useAuth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </HelmetProvider>
+);
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
